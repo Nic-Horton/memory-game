ba77431d64b2830890bc2f15f5c2132738b66dc6
Nic-Horton <nic.horton00@gmail.com>
2023-09-26 20:36:03 -0500

    Added functionality for flipping cards

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './MemoryCard.css'
+import { useState } from 'react'
 
 function MemoryCard() {
+  const [isFlipped,setIsFlipped] = useState(false);
+
+  const _clickHandler = () => {
+    setIsFlipped(!isFlipped)
+  }
+
   return (
-    <div className='MemoryCard'>
-      <div className='MemoryCardInner'>
+    <div className='MemoryCard' onClick={_clickHandler}>
+      <div className={isFlipped ? 'MemoryCardInner flipped' : 'MemoryCardInner'}>
         <div className='MemoryCardBack'>
           <img className='card-image' src="https://www.digitalcrafts.com/media/Default/assets/logos/dc-logo.svg" alt="" />
         </div>
