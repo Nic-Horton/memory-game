e08a3957e5d5c97a4620815224209cbd44e2c25d
Nic-Horton <nic.horton00@gmail.com>
2023-09-28 11:33:43 -0500

    Added shuffled deck generation on load and set up props to be passed to MemoryCard component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,51 @@
+import { useState } from 'react';
 import './App.css';
 import MemoryCard from './components/MemoryCard';
+import { useEffect } from 'react';
+
+const generateDeck = () => {
+	const symbols = ['∆', 'ß', '£', '§', '•', '$', '+', 'ø'];
+	const deck = [];
+
+	for (let i = 0; i < 16; i++) {
+		deck.push({ isFlipped: false, symbol: symbols[i % 8] });
+	}
+	shuffle(deck);
+	return deck;
+};
+
+const shuffle = (arr) => {
+	for (let i = arr.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[arr[i], arr[j]] = [arr[j], arr[i]];
+	}
+	return arr;
+};
 
 function App() {
+	const [deck, setDeck] = useState([]);
+	const [pickedCards, setPickedCards] = useState([]);
+
+	useEffect(() => {
+		setDeck(generateDeck());
+	}, [setDeck]);
+
+	let cardsJSX = deck.map((card, index) => {
+		return (
+			<MemoryCard symbol={card.symbol} isFlipped={card.isFlipped} key={index} />
+		);
+	});
+
 	return (
 		<div className="App">
 			<header className="App-header">
 				<h1>Memory Game</h1>
 				<h3 className="Sub-title">Match Cards To Win</h3>
 			</header>
-			<div>
-				<MemoryCard />
-				<MemoryCard />
-				<MemoryCard />
-				<MemoryCard />
-			</div>
-			<div>
-				<MemoryCard />
-				<MemoryCard />
-				<MemoryCard />
-				<MemoryCard />
-			</div>
-			<div>
-				<MemoryCard />
-				<MemoryCard />
-				<MemoryCard />
-				<MemoryCard />
-			</div>
-			<div>
-				<MemoryCard />
-				<MemoryCard />
-				<MemoryCard />
-				<MemoryCard />
-			</div>
+			<div>{cardsJSX.slice(0, 4)}</div>
+			<div>{cardsJSX.slice(4, 8)}</div>
+			<div>{cardsJSX.slice(8, 12)}</div>
+			<div>{cardsJSX.slice(12, 16)}</div>
 		</div>
 	);
 }
--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './MemoryCard.css'
 import { useState } from 'react'
 
-function MemoryCard() {
+function MemoryCard(props) {
   const [isFlipped,setIsFlipped] = useState(false);
 
   const _clickHandler = () => {
@@ -11,12 +11,12 @@ function MemoryCard() {
 
   return (
     <div className='MemoryCard' onClick={_clickHandler}>
-      <div className={isFlipped ? 'MemoryCardInner flipped' : 'MemoryCardInner'}>
+      <div className={props.isFlipped ? 'MemoryCardInner flipped' : 'MemoryCardInner'}>
         <div className='MemoryCardBack'>
           <img className='card-image' src="https://www.digitalcrafts.com/media/Default/assets/logos/dc-logo.svg" alt="" />
         </div>
         <div className='MemoryCardFront'>
-          ∆
+          {props.symbol}
         </div>
       </div>
     </div>
